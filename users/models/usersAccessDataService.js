5cdd28bda6119b91a418c6b7e60aa49ee0e7b222
RaulZvulunov <134226253+RaulZvulunov@users.noreply.github.com>
2024-06-04 22:07:50 +0300

    Add files via upload

---
+++ users/models/usersAccessDataService.js
@@ -0,0 +1,126 @@
+const DB = process.env.DB || "MONGODB";
+const {
+  generateAuthToken
+} = require("../../auth/Providers/jwt");
+const {
+  comparePassword
+} = require("../helpers/bcrypt");
+const User = require("./mongodb/User");
+const lodash = require("lodash");
+const registerUser = async normalizedUser => {
+  if (DB === "MONGODB") {
+    try {
+      const {
+        email
+      } = normalizedUser;
+      let user = await User.findOne({
+        email
+      });
+      if (user) throw new Error("User already registered");
+      user = new User(normalizedUser);
+      user = await user.save();
+      user = lodash.pick(user, ["name", "email", "_id"]);
+      return Promise.resolve(user);
+    } catch (error) {
+      error.status = 400;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("registerUser new user not in mongodb");
+};
+
+const loginUser = async ({
+  email,
+  password
+}) => {
+  if (DB === "MONGODB") {
+    try {
+      const user = await User.findOne({
+        email
+      });
+      if (!user) throw new Error("Invalid email or password");
+      const validPassword = comparePassword(password, user.password);
+      if (!validPassword) throw new Error("Invalid email or password");
+      const token = generateAuthToken(user)
+      return Promise.resolve(token);
+    } catch (error) {
+      error.status = 400;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("loginUser user not in mongodb");
+};
+
+const getUsers = async () => {
+  if (DB === "MONGODB") {
+    try {
+      const users = await User.find();
+      return Promise.resolve(users);
+    } catch (error) {
+      error.status = 404;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("get users not in mongodb");
+};
+
+const getUser = async userId => {
+  if (DB === "MONGODB") {
+    try {
+      //   throw new Error("Opss... i did it again!");
+      return Promise.resolve(`get user no: ${userId}`);
+    } catch (error) {
+      error.status = 404;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("get user not in mongodb");
+};
+
+const updateUser = async (userId, normalizedUser) => {
+  if (DB === "MONGODB") {
+    try {
+      return Promise.resolve({
+        normalizedUser,
+        userId
+      });
+    } catch (error) {
+      error.status = 400;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("card update not in mongodb");
+};
+
+const changeUserBusinessStatus = async userId => {
+  if (DB === "MONGODB") {
+    try {
+      return Promise.resolve(`user no. ${userId} change his business status!`);
+    } catch (error) {
+      error.status = 400;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("card liked not in mongodb");
+};
+
+const deleteUser = async userId => {
+  if (DB === "MONGODB") {
+    try {
+      return Promise.resolve(`user no. ${userId} deleted!`);
+    } catch (error) {
+      error.status = 400;
+      return Promise.reject(error);
+    }
+  }
+  return Promise.resolve("card deleted not in mongodb");
+};
+
+exports.registerUser = registerUser;
+exports.loginUser = loginUser;
+exports.getUsers = getUsers;
+exports.getUser = getUser;
+exports.updateUser = updateUser;
+exports.changeUserBusinessStatus = changeUserBusinessStatus;
+exports.deleteUser = deleteUser;
+
